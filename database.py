96109bdc25f503af00823ef67014a54e8428bc5d
Amit Kumar Sahni <sahniamitkumar2017@gmail.com>
2023-02-06 00:54:05 +0530

    Modified layout

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://nfmgzdce:3TsMnU7oEqd-fjP6gIoArl1J2ec42eH5@john.db.elephantsql.com/nfmgzdce"
-
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mysql97@127.0.0.1:3306/todoapp"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
